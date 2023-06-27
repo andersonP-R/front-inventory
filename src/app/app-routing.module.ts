7bffe9d2ce7ca6aa0c115f4789a130adc2dacfcb
Anderson Rivera <anderzonrivera480@gmail.com>
2023-06-26 21:37:19 -0500

    front inventory first version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { dashboardRoutingModule } from './modules/dashboard/dashboard-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { enableTracing: false, useHash: true }),
+    dashboardRoutingModule,
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
